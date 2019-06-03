530cd7692172dece28604588a025383c0a9c1ef5
poor <thezhukeeper@gmail.com>
2019-06-03 16:52:11 -0700

    semifinish project

--- first_rails_project/app/controllers/users_controller.rb
+++ first_rails_project/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
 
-  
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: params
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
+
